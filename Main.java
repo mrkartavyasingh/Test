454599c14246aacff18ce2ef372e9121d59a096b
mrkartavyasingh <mrkartavyasingh@gmail.com>
2024-05-21 10:41:09 +0530

    main 1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!! main 1");
         System.out.println("main1");
     }
 }
